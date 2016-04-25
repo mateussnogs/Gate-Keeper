826b257e2330d4d50ed120693b84f756cd22f222
mateussnogs <mateussnogs@gmail.com>
2016-04-24 22:01:53 -0300

    Walking legal

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class Fireball : MonoBehaviour {
-
+	public Amelia amelia;
+	public float speed = 10;
+	public GameObject target;
+	Vector3 origin;
+	Vector3 destiny;
+	public Vector3 targetDir;
 	// Use this for initialization
 	void Start () {
-	
+		origin = transform.position;
+		destiny = (target.transform.position - origin).normalized;
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame//
 	void Update () {
-	
+		//transform.position += targetDir * speed * Time.deltaTime;
+		Vector3 moveDir = (GetComponent<Renderer>().bounds.center - transform.position);
+		//ACHAR CENTRO DO TARGET
+		//Vector3 moveDir = (target.transform.position - transform.position);
+		transform.position = Vector3.MoveTowards (transform.position, moveDir, Time.deltaTime * speed);
 	}
 }
--- Assets/Scripts/Wyvern.cs
+++ Assets/Scripts/Wyvern.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class Wyvern : MonoBehaviour {
+	public Amelia amelia;
+	public Fireball fireball;
+	public float shootCooldown;
+	float shootTimeAcc = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +14,26 @@ public class Wyvern : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void FixedUpdate() {
+		Attack ();
+	}
+
+	void Attack() {
+		if (Time.time > shootTimeAcc) {
+			Shoot (amelia.gameObject);
+			shootTimeAcc = Time.time + shootCooldown;
+		}
+	}
+
+	void Shoot(GameObject go) {		
+		Fireball fb = Instantiate (fireball, transform.position, Quaternion.identity) as Fireball;
+		fb.target = go;
+		fb.targetDir = (go.transform.position - transform.position).normalized;
+		/*float speed = fb.speed;
+		fb.GetComponent<Rigidbody2D> ().velocity = go.transform.position - transform.position * speed;*/
+
 	}
 }
