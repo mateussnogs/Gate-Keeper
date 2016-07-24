b7b2c91d5c6beccd92108a04266304ab582fd806
Danilo Farias Vettorazzi <danfvettor@gmail.com>
2016-07-24 13:41:54 -0300

    Background, Movimento da Amélia

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    public PlayerMovement playerMovement;
+    private Vector2 dir;
+
+    void Update()
+    {
+        KeyboardInput();
+    }
+    private void KeyboardInput()
+    {
+        dir = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        playerMovement.Move(dir);
+    }
+}
