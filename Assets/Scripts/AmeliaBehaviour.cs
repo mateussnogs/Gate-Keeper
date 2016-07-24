d339dc4d653ab59f3fcb728aa90f0c75b59beac4
Danilo Farias Vettorazzi <danfvettor@gmail.com>
2016-07-24 16:54:58 -0300

    Organizada nas pastas, UI com movimento funcionando

---
+++ Assets/Scripts/AmeliaBehaviour.cs
@@ -0,0 +1,21 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class AmeliaBehaviour: MonoBehaviour {
+
+    private Vector2 dir;
+    public Rigidbody2D rb;
+    public float speed;
+
+    public void Move(float dirX)
+    {
+        dir = new Vector2(dirX, 0);
+        rb.velocity = dir.normalized * speed;
+    }
+
+    public void Attack()
+    {
+        print("Ataquei");
+    }
+
+}
