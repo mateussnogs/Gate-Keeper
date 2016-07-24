97c0272e0fa21cae696d0219eb92b8df24d144fb
Danilo Farias Vettorazzi <danfvettor@gmail.com>
2016-07-24 20:41:09 -0300

    Lógica da maquina de estado do inimigo implementada

---
+++ Assets/IEnemyStates.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IEnemyStates : MonoBehaviour {
+    private Vector2 newPos;
+    //diferente da velocidade do player, quero saber o tamanho do passo
+    public float speed;
+    
+    public IEnemyStates(float speed)
+    {
+        this.speed = speed;
+    }
+
+	public void Pursue(Transform target, Transform enemyPos)
+    {
+        newPos = new Vector2(target.position.x, enemyPos.position.y);
+        enemyPos.position = Vector2.Lerp(enemyPos.position, newPos, speed);
+    }
+    public void Attack()
+    {
+        print("Ataque");
+    }
+
+}
---
+++ Assets/StateMachine.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour {
+    private GameObject gate, player;
+    private Transform target;
+    private float gateDistance, playerDistance;
+    private IEnemyStates states;
+
+    public EnemyCollisionTreatment enemy;
+    public Transform enemyTransform;
+   
+    void Start()
+    {
+        states = new IEnemyStates(enemy.speed);
+
+        gate = GameObject.FindGameObjectWithTag("Gate");
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        StartCoroutine(CheckState());
+      
+}
+    IEnumerator CheckState()
+    {
+        while (true)
+        {
+            if (enemy.isPursuing)
+            {
+                AttackToPursue();
+            }
+            else if (enemy.isAttacking)
+            {
+                PursueToAttack();
+            }
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    void PursueToAttack()
+    {
+        states.Attack();
+    }
+    void AttackToPursue()
+    {
+
+        enemy.isAttacking = false;
+        enemy.isPursuing = true;
+
+        gateDistance = Mathf.Abs(gate.transform.position.x - enemyTransform.position.x);
+        playerDistance = Mathf.Abs(player.transform.position.x - enemyTransform.position.x);
+
+        if (gateDistance > playerDistance) states.Pursue(player.transform, enemyTransform.transform);
+        else states.Pursue(gate.transform, enemyTransform.transform);
+        
+    }
+  
+
+}
