747b7e5aa12b8f54cb20c1cb0e3493cd62e51c20
danvettor <danfvettor@gmail.com>
2016-07-26 18:29:51 -0300

    Amelia destrói Knight quando ataca (imperfeito)

--- Assets/Scripts/AmeliaBehaviour.cs
+++ Assets/Scripts/AmeliaBehaviour.cs
@@ -2,22 +2,49 @@
 using System.Collections;
 
 public class AmeliaBehaviour: MonoBehaviour {
+    public float speed;
 
     private Vector2 dir;
-    public Rigidbody2D rb;
-    public float speed;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private IEnumerator coroutineInstance;
+
+    [HideInInspector]
+    public bool isAttacking;
+
+    void Start()
+    {
+        coroutineInstance = null;
+        rb = GetComponentInParent<Rigidbody2D>();
+        anim = GetComponentInParent<Animator>();
+        isAttacking = false;
+    }
 
     public void Move(float dirX)
     {
         dir = new Vector2(dirX, 0);
         rb.velocity = dir.normalized * speed;
     }
-
-    public void Attack()
+    public void AttackTrigger()
     {
-        print("Ataquei");
+        if (coroutineInstance == null)
+        {
+            coroutineInstance = Attack();
+            StartCoroutine(coroutineInstance);
+        }
     }
+    public IEnumerator Attack()
+    {
+        anim.SetTrigger("Attack");
+        isAttacking = true;
+        //pega o tempo do ataque pra habilitar novamente
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorClipInfo(0)[0].clip.length);
 
+        coroutineInstance = null;
+        isAttacking = false;
+
+    }
+    
 
 
 }
--- Assets/Scripts/EnemyCollisionTreatment.cs
+++ Assets/Scripts/EnemyCollisionTreatment.cs
@@ -15,18 +15,37 @@ public class EnemyCollisionTreatment : MonoBehaviour {
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(!(col.gameObject.CompareTag("Enemy")))
+        if(col.gameObject.CompareTag("Gate"))
         {
             isAttacking = true;
             isPursuing = false;
         }
-    }
-    void OnTriggerStay2D(Collider2D col)
-    {
-        if (!(col.gameObject.CompareTag("Enemy")))
+        else if(col.gameObject.CompareTag("Player"))
         {
             isAttacking = true;
             isPursuing = false;
+            if (col.GetComponentInChildren<AmeliaBehaviour>().isAttacking)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Gate"))
+        {
+            isAttacking = true;
+            isPursuing = false;
+        }
+        else if (col.gameObject.CompareTag("Player"))
+        {
+            isAttacking = true;
+            isPursuing = false;
+            if (col.GetComponentInChildren<AmeliaBehaviour>().isAttacking)
+            {
+                print("Destrui");
+                Destroy(transform.parent.gameObject);
+            }
         }
     }
     void OnTriggerExit2D(Collider2D col)
